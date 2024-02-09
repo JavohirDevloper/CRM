16aec4f6845c11161c2f53c8a396408e803561dc
JavohirDevloper <muhammadiyevj72@gmail.com>
2024-02-09 20:34:33 +0500

    crm uchun seeds

---
+++ backend/src/seed/index.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const config = require("../shared/config");
+
+const Admin = require("../models/Admin");
+
+const AdminSeed = require("./admin-seed");
+
+const seedData = async () => {
+  const uri = process.env.DB;
+  mongoose.set("strictQuery", false);
+  mongoose
+    .connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connected to db");
+    })
+    .catch((err) => {
+      console.log("error", err);
+    });
+
+  const seedDB = async () => {
+    await Admin.deleteMany({});
+    await Admin.insertMany(AdminSeed);
+  };
+
+  seedDB().then(() => {
+    mongoose.connection.close();
+  });
+};
+
+seedData();
