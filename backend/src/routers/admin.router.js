909f7d40af52d1a87aad0e2f37ad39af35f0c191
JavohirDevloper <muhammadiyevj72@gmail.com>
2024-02-10 17:39:18 +0500

    admin routeslari

---
+++ backend/src/routers/admin.router.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const AdminController = require("../controllers/admin.controller");
+const isLoggedIn = require("../shared/auth/isLoggedIn");
+
+// admin login
+router.post("/admin/login", AdminController.AdminLogin);
+
+// Admin
+router.post("/admin", isLoggedIn, AdminController.createAdmin);
+router.get("/admin", isLoggedIn, AdminController.getAllAdmins);
+router.get("/admin/:id", isLoggedIn, AdminController.getAdminById);
+router.put("/admin/:id", isLoggedIn, AdminController.updateAdmin);
+router.delete("/admin/:id", isLoggedIn, AdminController.deleteAdmin);
+
+module.exports = router;
