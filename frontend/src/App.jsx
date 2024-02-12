83e51e2a77ca1ae443a1ec317835370ab8b16bdc
JavohirDevloper <muhammadiyevj72@gmail.com>
2024-02-12 21:07:56 +0500

    react componentlar ulandi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard";
+import Register from "./components/Register/Register";
+import Verify from "./components/Verify/Verify";
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/user/register" element={<Register/>}/>
+        <Route path="/user/verify" element={<Verify/>}/>
       </Routes>
     </>
   );
