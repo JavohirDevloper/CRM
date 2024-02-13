367fd8fb85ed21db40b4009837238d7626b615f3
JavohirDevloper <muhammadiyevj72@gmail.com>
2024-02-13 20:51:57 +0500

    user router

--- backend/src/routers/user.router.js
+++ backend/src/routers/user.router.js
@@ -3,14 +3,23 @@ const router = express.Router();
 const UserController = require("../controllers/user.controller");
 const isLoggedIn = require("../shared/auth/isLoggedIn");
 
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  message:
+    "Foydalanuvchi hajmi limitga yetdi. Iltimos, keyinroq harakat qiling.",
+});
+
 // user authentication
 router.post("/user/register", UserController.registerAndLoginUser);
 router.post("/user/verify", UserController.userLogin);
 router.get("/user/code/:code", UserController.getTokenByCode);
 
- // userlar uchun router
-router.get("/user/:id", isLoggedIn, UserController.getUserById);
-router.put("/user/:id", isLoggedIn, UserController.updateUser);
-router.delete("/user/:id", isLoggedIn, UserController.deleteUser);
+// userlar uchun router
+router.get("/user/:id", isLoggedIn, limiter, UserController.getUserById);
+router.put("/user/:id", isLoggedIn, limiter, UserController.updateUser);
+router.delete("/user/:id", isLoggedIn, limiter, UserController.deleteUser);
 
 module.exports = router;
