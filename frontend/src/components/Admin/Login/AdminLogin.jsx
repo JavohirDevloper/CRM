fc08564e712ee11f457fdd796399d6e4a31e5047
JavohirDevloper <muhammadiyevj72@gmail.com>
2024-02-13 22:03:34 +0500

    admin login qilishi

---
+++ frontend/src/components/Admin/Login/AdminLogin.jsx
@@ -0,0 +1,56 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-toastify";
+import "./adminlogin.css";
+const AdminLogin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:5000/admin/login", {
+        email,
+        password,
+      });
+      localStorage.setItem("adminToken", response.data.token);
+      toast.success("Siz endi adminsiz :)");
+      navigate("/admin");
+    } catch (error) {
+      setError(error.response.data.message);
+      toast.error(error);
+    }
+  };
+
+  return (
+    <div className="admin-login-container">
+      <h2>Admin Login</h2>
+      {error && <div className="error">{error}</div>}
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default AdminLogin;
