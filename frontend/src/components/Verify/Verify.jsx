6484eaecc5e13555b3e188bdb4bf39e39a62b043
JavohirDevloper <muhammadiyevj72@gmail.com>
2024-02-13 20:53:50 +0500

    verify uchun bildirishnomalar qoʻshildi

--- frontend/src/components/Verify/Verify.jsx
+++ frontend/src/components/Verify/Verify.jsx
@@ -1,14 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
 import axios from "axios";
-import {useNavigate } from "react-router-dom"
-import "./verify.css"
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import "./verify.css";
 const Verify = () => {
   const [email, setEmail] = useState("");
   const [verificationCode, setVerificationCode] = useState("");
   const [message, setMessage] = useState("");
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleRegister = async () => {
     try {
@@ -18,8 +19,10 @@ const Verify = () => {
       if (response.status === 201) {
         setMessage("Tasdiqlash kodi emailga yuborildi.");
       }
+      toast.success("Sizning gmailingizga kod bordi ");
     } catch (error) {
       console.error("Error registering:", error);
+      toast.error(error.message);
     }
   };
 
@@ -32,22 +35,24 @@ const Verify = () => {
       console.log(response);
       if (response.status === 200) {
         setMessage("Tasdiqlandi! Bosh sahifaga oʻting.");
-        navigate("/"); 
+        navigate("/");
       }
+      toast.success("Siz tizimga kirdizngiz !");
     } catch (error) {
       console.log("Error verifying:", error);
+      toast.error(error);
     }
   };
 
   return (
     <div className="container">
-      <h1>Ro'yxatdan o'tish</h1>
+      <h1>Roʻyxatdan oʻtish</h1>
       <input
         type="email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
-      <button onClick={handleRegister}>Ro'yxatdan o'tish</button>
+      <button onClick={handleRegister}>Roʻyxatdan oʻtish</button>
 
       <h2>Tasdiqlash kodini kiriting</h2>
       <input
