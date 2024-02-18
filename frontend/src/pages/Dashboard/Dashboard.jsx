4ed10e3eaefc55523d1b3750c20c6e7dff63f320
Javohir Muhammadiyev <138434351+JavohirDevloper@users.noreply.github.com>
2024-02-18 19:42:04 +0500

    dashboard page

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,15 +1,17 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
-import Navbar from '../../components/Home/Navbar/Navbar'
-import Xarita from '../../components/Home/Xarita/Xarita'
+import React from "react";
+import Navbar from "../../components/Home/Navbar/Navbar";
+// import Xarita from "../../components/Home/Xarita/Xarita";
+import Sidebar from "../../components/Home/Sidebar/Sidebar";
 
 const Dashboard = () => {
   return (
     <div>
-        <Navbar/>
-        <Xarita/>
+      <Navbar />
+      <Sidebar />
+      {/* <Xarita /> */}
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
