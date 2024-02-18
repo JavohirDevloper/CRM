351fed005156eab2f5f132764e14461d5fc059f0
Javohir Muhammadiyev <138434351+JavohirDevloper@users.noreply.github.com>
2024-02-18 19:42:11 +0500

    notfound page

---
+++ frontend/src/components/NotFound/NotFound.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import notfound from "../../../public/404.png"
+const NotFound = () => {
+  return (
+    <div>
+        <img src={notfound} alt="" />
+    </div>
+  )
+}
+
+export default NotFound
