b6da6dd627b1dd8191fe4abdeefe2b4f9958a85c
Javohir Muhammadiyev <138434351+JavohirDevloper@users.noreply.github.com>
2024-02-20 19:17:44 +0500

    user phone model

---
+++ backend/src/models/UserPhone.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+
+const UserPhoneSchema = new mongoose.Schema(
+  {
+    first_name: {
+      type: mongoose.SchemaTypes.String,
+      required: true,
+    },
+    last_name: {
+      type: mongoose.SchemaTypes.String,
+    },
+    phone_number: {
+      type: mongoose.SchemaTypes.String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: mongoose.SchemaTypes.String,
+      required: true,
+    },
+    images: {
+      type: mongoose.SchemaTypes.String,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
+    gender: {
+      type: mongoose.SchemaTypes.Boolean,
+      enum: ["male", "female"],
+    },
+    date_of_birth: {
+      type: mongoose.SchemaTypes.Date,
+    },
+    role: {
+      type: mongoose.SchemaTypes.String,
+      enum: ["user", "admin", "students"],
+      default: "user",
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+      deletedAt: "deleted_at",
+    },
+  }
+);
+
+const UserPhone = mongoose.model("UserPhone", UserPhoneSchema);
+module.exports = UserPhone;
