7a69320deb985f16512d577fa53bb5ecdcccb9bb
Javohir Muhammadiyev <138434351+JavohirDevloper@users.noreply.github.com>
2024-02-20 15:51:31 +0500

    index router

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,18 +5,19 @@ const bodyParser = require("body-parser");
 const db = require("./db/db");
 const handleError = require("./shared/errors/handle");
 const useragent = require("express-useragent");
-const path = require("path")
+const path = require("path");
 // user routerlari
 const UserRouter = require("./routers/user.router");
 const AdminRouter = require("./routers/admin.router");
 const FileRouter = require("./routers/files.router");
 const CoursesRouter = require("./routers/courses.router");
+const TeacherRouter = require("./routers/teacher.router");
 dotenv.config();
 const app = express();
 // app use
 app.use(express.json());
-app.get("/videos/:filename",(req,res) =>{
-  res.sendFile(path.join(__dirname,"..","videos",req.params.filename))
+app.get("/videos/:filename", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "videos", req.params.filename));
 });
 app.use(cors());
 app.use(bodyParser.json());
@@ -27,6 +28,7 @@ app.use(UserRouter);
 app.use(AdminRouter);
 app.use(FileRouter);
 app.use(CoursesRouter);
+app.use(TeacherRouter);
 // databaza
 db();
 
