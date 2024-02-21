ee5df96bc2ae5d05eb39ee2ca1e2c564967118fc
Javohir Muhammadiyev <138434351+JavohirDevloper@users.noreply.github.com>
2024-02-21 11:59:17 +0500

    hasRole teacher

--- backend/src/routers/teacher.router.js
+++ backend/src/routers/teacher.router.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const TeacherController = require("../controllers/teacher.controller");
-// const isLoggedIn = require("../shared/auth/isLoggedIn");
+const isLoggedIn = require("../shared/auth/isLoggedIn");
+const hasRole = require("../shared/auth/hasRole");
 
-// Teacher login
+// teacher login
 router.post("/teacher/login", TeacherController.TeacherLogin);
 
-// Teacher
-router.post("/teacher", TeacherController.createTeacher);
-router.get("/teacher", TeacherController.getAllTeachers);
-router.put("/teacher/:id", TeacherController.updateTeacher);
-router.delete("/teacher/:id", TeacherController.deleteTeacher);
+// teacher
+router.post("/teacher",isLoggedIn, hasRole(["admin", "teacher"]),  TeacherController.createTeacher);
+router.get("/teacher", isLoggedIn, hasRole(["admin", "teacher"]), TeacherController.getAllTeachers);
+router.put("/teacher/:id",isLoggedIn, hasRole(["admin", "teacher"]),  TeacherController.updateTeacher);
+router.delete("/teacher/:id",isLoggedIn, hasRole(["admin", "teacher"]),  TeacherController.deleteTeacher);
 
 module.exports = router;
