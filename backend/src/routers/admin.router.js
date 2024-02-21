a1e6518c5394c1ae675c7037da6a0107e5ae9819
Javohir Muhammadiyev <138434351+JavohirDevloper@users.noreply.github.com>
2024-02-21 12:16:44 +0500

    admin router hasRole

--- backend/src/routers/admin.router.js
+++ backend/src/routers/admin.router.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const AdminController = require("../controllers/admin.controller");
-// const isLoggedIn = require("../shared/auth/isLoggedIn");
-
+const isLoggedIn = require("../shared/auth/isLoggedIn");
+const hasRole = require("../shared/auth/hasRole");
 // admin login
 router.post("/admin/login", AdminController.AdminLogin);
 
 // Admin
-router.post("/admin", AdminController.createAdmin);
-router.get("/admin", AdminController.getAllAdmins);
-router.put("/admin/:id", AdminController.updateAdmin);
-router.delete("/admin/:id", AdminController.deleteAdmin);
+router.post("/admin",isLoggedIn, hasRole(["admin"]), AdminController.createAdmin);
+router.get("/admin",isLoggedIn, hasRole(["admin"]),  AdminController.getAllAdmins);
+router.put("/admin/:id",isLoggedIn, hasRole(["admin"]),  AdminController.updateAdmin);
+router.delete("/admin/:id",isLoggedIn, hasRole(["admin"]),  AdminController.deleteAdmin);
 
 module.exports = router;
