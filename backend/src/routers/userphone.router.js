5d0520bae9f29cdd5b24b4d599f8d0bfcce8e234
Javohir Muhammadiyev <138434351+JavohirDevloper@users.noreply.github.com>
2024-02-21 11:58:56 +0500

    hasRole userphone

--- backend/src/routers/userphone.router.js
+++ backend/src/routers/userphone.router.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const router = express.Router();
+const rateLimit = require("express-rate-limit");
 const UserController = require("../controllers/userphone.controller.js");
 const isLoggedIn = require("../shared/auth/isLoggedIn");
+const hasRole = require("../shared/auth/hasRole");
 
-const rateLimit = require("express-rate-limit");
-
+const router = express.Router();
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000,
   max: 30,
@@ -17,8 +17,26 @@ router.post("/user/register", UserController.registerAndLoginUser);
 router.get("/user/code/:code", UserController);
 
 // userlar uchun router
-router.get("/user/:id", isLoggedIn, limiter, UserController.getUserById);
-router.put("/user/:id", isLoggedIn, limiter, UserController.updateUser);
-router.delete("/user/:id", isLoggedIn, limiter, UserController.deleteUser);
+router.get(
+  "/user/:id",
+  isLoggedIn,
+  hasRole(["admin", "students"]),
+  limiter,
+  UserController.getUserById
+);
+router.put(
+  "/user/:id",
+  isLoggedIn,
+  hasRole(["admin", "students"]),
+  limiter,
+  UserController.updateUser
+);
+router.delete(
+  "/user/:id",
+  isLoggedIn,
+  hasRole(["admin", "students"]),
+  limiter,
+  UserController.deleteUser
+);
 
 module.exports = router;
