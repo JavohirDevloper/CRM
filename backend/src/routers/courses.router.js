a20546e538b96399460fe18a8411d12c587e1638
Javohir Muhammadiyev <138434351+JavohirDevloper@users.noreply.github.com>
2024-02-21 12:16:57 +0500

    courses router isLogggedin

--- backend/src/routers/courses.router.js
+++ backend/src/routers/courses.router.js
@@ -4,10 +4,10 @@ const isLoggedIn = require("../shared/auth/isLoggedIn");
 const hasRole = require("../shared/auth/hasRole");
 const router = express.Router();
 
-router.post("/courses", hasRole(["admin"]), coursesController.createCourse);
-router.get("/courses", hasRole(["admin", "teacher", "students"]),coursesController.getAllCourses);
-router.get("/courses/:id", hasRole(["admin", "teacher", "students"]),coursesController.getCourseById);
-router.put("/courses/:id",hasRole(["admin"]), coursesController.updateCourseById);
-router.delete("/courses/:id", hasRole(["admin"]),coursesController.deleteCourseById);
+router.post("/courses",isLoggedIn, hasRole(["admin"]), coursesController.createCourse);
+router.get("/courses",isLoggedIn, hasRole(["admin", "teacher", "students"]),coursesController.getAllCourses);
+router.get("/courses/:id",isLoggedIn, hasRole(["admin", "teacher", "students"]),coursesController.getCourseById);
+router.put("/courses/:id",isLoggedIn, hasRole(["admin"]), coursesController.updateCourseById);
+router.delete("/courses/:id",isLoggedIn, hasRole(["admin"]),coursesController.deleteCourseById);
 
 module.exports = router;
