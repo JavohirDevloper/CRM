b42f19b48d75983abbdefbf0b6e39df998a99baf
Javohir Muhammadiyev <138434351+JavohirDevloper@users.noreply.github.com>
2024-02-22 16:06:21 +0500

    app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Register from "./components/Register/Register";
+import Sigup from "./components/Auth/Register"
 import Verify from "./components/Verify/Verify";
 import Admin from "./components/Admin/Admin/Admin";
 import AdminLogin from "./components/Admin/Login/AdminLogin";
@@ -29,6 +30,7 @@ const App = () => {
         <Route path="/courses" element={<Courses />} />
         <Route path="/courses/create" element={<CreateCourse />} />
         <Route path="/*" element={<NotFound />} />
+        <Route path="/auth/register" element={<Sigup/>}/>
       </Routes>
     </>
   );
