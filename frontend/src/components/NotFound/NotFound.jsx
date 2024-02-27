2350127316d6ead1aecce3875e339dbb55e6e9ec
Javohir Muhammadiyev <138434351+JavohirDevloper@users.noreply.github.com>
2024-02-27 19:23:03 +0500

    not found page

--- frontend/src/components/NotFound/NotFound.jsx
+++ frontend/src/components/NotFound/NotFound.jsx
@@ -1,10 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import notfound from "../../../public/404.png"
+import "./notfound.css"
 const NotFound = () => {
   return (
     <div>
-        <img src={notfound} alt="" />
+      <h2>404 Not Found</h2>
+        <img  className="center" src={notfound} alt="" />
     </div>
   )
 }
