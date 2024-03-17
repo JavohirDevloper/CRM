0373d99d44ecbe4f0e2f083a8ab77a94abb6fe1b
Javohir Muhammadiyev <138434351+JavohirDevloper@users.noreply.github.com>
2024-03-17 14:07:28 +0500

    teacher router

--- backend/src/routers/teacher.router.js
+++ backend/src/routers/teacher.router.js
@@ -1,16 +1,38 @@
 const express = require("express");
 const router = express.Router();
-const TeacherController = require("../controllers/teacher.controller");
+const {
+  createTeacher,
+  getAllTeachers,
+  updateTeacher,
+  deleteTeacher,
+  login,
+  register,
+  getTeacherById,
+} = require("../controllers/teacher.controller");
 const isLoggedIn = require("../shared/auth/isLoggedIn");
 const hasRole = require("../shared/auth/hasRole");
+const isMongoId = require("../shared/validator/isMongoId");
 
-// teacher login
-router.post("/teacher/login", TeacherController.TeacherLogin);
+const rateLimit = require("express-rate-limit");
 
-// teacher
-router.post("/teacher",isLoggedIn, hasRole(["admin", "teacher"]),  TeacherController.createTeacher);
-router.get("/teacher", isLoggedIn, hasRole(["admin", "teacher"]), TeacherController.getAllTeachers);
-router.put("/teacher/:id",isLoggedIn, hasRole(["admin", "teacher"]),  TeacherController.updateTeacher);
-router.delete("/teacher/:id",isLoggedIn, hasRole(["admin", "teacher"]),  TeacherController.deleteTeacher);
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 30,
+  message:
+    "Foydalanuvchi hajmi limitga yetdi. Iltimos, keyinroq harakat qiling.",
+});
+
+const mTeacherCreate = [isLoggedIn, limiter, hasRole(["admin"])];
+const mFindTeacher = [isLoggedIn,limiter, hasRole(["admin", "teacher"])]
+const mFindByIdTeacher = [isLoggedIn,limiter, isMongoId, hasRole(["admin", "teacher"])]
+const mUpdateByIdTeacher = [isLoggedIn,limiter, isMongoId, hasRole(["admin", "teacher"])]
+const mDeleteTeacher = [isLoggedIn, limiter, isMongoId, hasRole(["teacher", "admin"])]
+
+router.post("/teacher/login", login);
+router.post("/teacher", mTeacherCreate, createTeacher);
+router.get("/teacher",mFindTeacher, getAllTeachers);
+router.get("/teacher/:id", mFindByIdTeacher, getTeacherById)
+router.put("/teacher/:id",mUpdateByIdTeacher, updateTeacher);
+router.delete("/teacher/:id",mDeleteTeacher, deleteTeacher);
 
 module.exports = router;
