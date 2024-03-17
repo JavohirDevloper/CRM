2e866f2170af9a291ec62360b231876608436e8e
Javohir Muhammadiyev <138434351+JavohirDevloper@users.noreply.github.com>
2024-03-17 22:32:40 +0500

    Files models

--- backend/src/models/File.js
+++ backend/src/models/File.js
@@ -13,10 +13,6 @@ const fileSchema = new mongoose.Schema({
     enum: ["mp4", "mp3"],
     default: "mp4",
   },
-  is_deleted: {
-    type: mongoose.SchemaTypes.Boolean,
-    default: false,
-  },
 });
 
 const File = mongoose.model("File", fileSchema);
