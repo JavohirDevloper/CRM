8227b4c9290062d25cc5d2cb3916ecf7bde239c5
Javohir Muhammadiyev <138434351+JavohirDevloper@users.noreply.github.com>
2024-03-17 22:34:09 +0500

    Message models

---
+++ backend/src/models/Message.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    message: {
+      type:  mongoose.Schema.Types.String,
+      trim: true,
+    },
+    chatId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Chat",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const messageModel = mongoose.model("Message", messageSchema);
+
+module.exports = { messageModel };
