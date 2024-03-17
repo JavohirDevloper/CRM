13facb66d39dd37910267fda49751a4cc74cd3be
Javohir Muhammadiyev <138434351+JavohirDevloper@users.noreply.github.com>
2024-03-17 22:34:18 +0500

    file controller

--- backend/src/controllers/file.controller.js
+++ backend/src/controllers/file.controller.js
@@ -97,7 +97,7 @@ const updateFile = async (req, res) => {
       new: true,
     });
     if (!updatedFile) {
-      return res.status(404).json({ error });
+      return res.status(404).json({ error: "File not found" });
     }
     res.json(updatedFile);
   } catch (error) {
@@ -107,10 +107,48 @@ const updateFile = async (req, res) => {
 
 const deleteFile = async (req, res) => {
   try {
-    await File.findByIdAndDelete(req.params.id);
-    res.json({ message: "File deleted successfully" });
+    const { id } = req.params;
+    const deletedAdmin = await File.findByIdAndDelete(id, { is_deleted: true });
+    if (!deletedAdmin) {
+      return res.status(404).json({ error: "File not found" });
+    }
+    res.status(200).json({ message: deletedAdmin });
   } catch (error) {
-    res.status(500).json({ error });
+    res.status(400).json({ error: error.message });
+  }
+};
+const streamFile = (req, res) => {
+  const { filename } = req.params;
+  const filePath = "./videos/" + filename;
+  const stat = fs.statSync(filePath);
+  let fileSize = stat.size;
+
+  const reductionFactor = 0.5;
+  fileSize = Math.floor(fileSize * reductionFactor);
+
+  const range = req.headers.range;
+
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-");
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+    const chunksize = end - start + 1;
+    const file = fs.createReadStream(filePath, { start, end });
+    const head = {
+      "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": chunksize,
+      "Content-Type": "video/mp4",
+    };
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      "Content-Length": fileSize,
+      "Content-Type": "video/mp4",
+    };
+    res.writeHead(200, head);
+    fs.createReadStream(filePath).pipe(res);
   }
 };
 module.exports = {
@@ -120,4 +158,5 @@ module.exports = {
   deleteFile,
   upload,
   getAllFile,
+  streamFile,
 };
