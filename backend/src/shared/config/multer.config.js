15ae2fd588e4c65c9e09cb2f9e93945c22913177
Javohir Muhammadiyev <138434351+JavohirDevloper@users.noreply.github.com>
2024-03-17 14:06:23 +0500

    multer

--- backend/src/shared/config/multer.config.js
+++ backend/src/shared/config/multer.config.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "/upload/");
+    cb(null, "public/");
   },
   filename: function (req, file, cb) {
     const trimmedFileName = file.originalname.replace(/\s+/g, "").toLowerCase();
@@ -14,7 +14,7 @@ const fileFilter = (req, file, cb) => {
   if (file.mimetype.startsWith("video/")) {
     cb(null, true);
   } else {
-    cb(new Error("Faqat video fayllariga ruxsat beriladi!"), false);
+    cb(new Error("Only images are allowed!"), false);
   }
 };
 
