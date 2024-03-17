94559c2c048f14088ad9bf334f2dd1668e7fbb43
Javohir Muhammadiyev <138434351+JavohirDevloper@users.noreply.github.com>
2024-03-17 14:08:00 +0500

    User module

---
+++ backend/src/models/User.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const UserEmailSchema = new mongoose.Schema({
+  first_name: {
+    type: mongoose.SchemaTypes.String,
+    required: true,
+  },
+  last_name: {
+    type: mongoose.SchemaTypes.String,
+    required: true,
+  },
+  email: {
+    type: mongoose.SchemaTypes.String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: mongoose.SchemaTypes.String,
+    required: true,
+  },
+  images: {
+    type: mongoose.SchemaTypes.String,
+    default:
+      "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+  },
+  gender: {
+    type: mongoose.SchemaTypes.Boolean,
+    enum: ["male", "female"],
+  },
+  role: {
+    type: mongoose.SchemaTypes.String,
+    default: "student",
+  },
+  is_deleted: {
+    type: mongoose.SchemaTypes.String,
+    enum: ["true", "false"],
+    default: "false",
+  },
+});
+
+const UserEmail = mongoose.model("User", UserEmailSchema);
+module.exports = UserEmail;
