950a94f3d0c25e2872460005a483b531b8b635f3
Javohir Muhammadiyev <138434351+JavohirDevloper@users.noreply.github.com>
2024-03-17 14:07:45 +0500

    courses router

--- backend/src/routers/courses.router.js
+++ backend/src/routers/courses.router.js
@@ -1,13 +1,33 @@
 const express = require("express");
-const coursesController = require("../controllers/courses.controller");
+const router = express.Router();
+const {
+  createCourse,
+  getAllCourses,
+  getCourseById,
+  updateCourseById,
+  deleteCourseById,
+} = require("../controllers/courses.controller");
 const isLoggedIn = require("../shared/auth/isLoggedIn");
 const hasRole = require("../shared/auth/hasRole");
-const router = express.Router();
+const isMongoId = require("../shared/validator/isMongoId");
+const rateLimit = require("express-rate-limit");
 
-router.post("/courses",isLoggedIn, hasRole(["admin"]), coursesController.createCourse);
-router.get("/courses",isLoggedIn, hasRole(["admin", "teacher", "students"]),coursesController.getAllCourses);
-router.get("/courses/:id",isLoggedIn, hasRole(["admin", "teacher", "students"]),coursesController.getCourseById);
-router.put("/courses/:id",isLoggedIn, hasRole(["admin"]), coursesController.updateCourseById);
-router.delete("/courses/:id",isLoggedIn, hasRole(["admin"]),coursesController.deleteCourseById);
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 30,
+  message:
+    "Foydalanuvchi hajmi limitga yetdi. Iltimos, keyinroq harakat qiling.",
+});
+const mCoursesCreate = [isLoggedIn, limiter, hasRole(["admin"])];
+const mGetCourses = [isLoggedIn, limiter, hasRole(["student", "admin"])];
+const mGetCorsesById = [isLoggedIn, limiter, isMongoId, hasRole(["admin"])];
+const mUpdateCorsesById = [isLoggedIn, limiter, isMongoId, hasRole(["admin"])];
+const mDeleteCorsesById = [isLoggedIn, limiter, isMongoId, hasRole(["admin"])];
+
+router.post("/courses", mCoursesCreate, createCourse);
+router.get("/courses", mGetCourses, getAllCourses);
+router.get("/courses/:id", mGetCorsesById, getCourseById);
+router.put("/courses/:id", mUpdateCorsesById, updateCourseById);
+router.delete("/courses/:id", mDeleteCorsesById, deleteCourseById);
 
 module.exports = router;
