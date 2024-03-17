9e61c0bafa7158b2150b3ad57271cd737be5c6d9
Javohir Muhammadiyev <138434351+JavohirDevloper@users.noreply.github.com>
2024-03-17 14:07:36 +0500

    files router

--- backend/src/routers/files.router.js
+++ backend/src/routers/files.router.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const rateLimit = require("express-rate-limit");
 const router = express.Router();
-const fileController = require("../controllers/file.controller.js");
+const {
+  createFile,
+  getAllFile,
+  updateFile,
+  deleteFile,
+} = require("../controllers/file.controller.js");
 const isLoggedIn = require("../shared/auth/isLoggedIn");
 const hasRole = require("../shared/auth/hasRole");
+const isMongoId = require("../shared/validator/isMongoId");
+
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000,
   max: 35,
@@ -11,33 +18,14 @@ const limiter = rateLimit({
     "Foydalanuvchi hajmi limitga yetdi. Iltimos, keyinroq harakat qiling.",
 });
 
-router.post(
-  "/videos",
-  isLoggedIn,
-  limiter,
-  fileController.createFile
-);
+const mCreateVideo = [isLoggedIn, limiter, hasRole(["admin"])];
+const mFindVideo = [isLoggedIn, limiter, hasRole(["student", "admin"])];
+const mUpdateVideo = [isLoggedIn, limiter, isMongoId, hasRole(["admin"])];
+const mDeleteVideo = [isLoggedIn, limiter, isMongoId, hasRole(["admin"])];
 
-router.get(
-  "/videos",
-  isLoggedIn,
-  hasRole(["admin", "students"]),
-  limiter,
-  fileController.getAllFile
-);
-router.put(
-  "/videos/:id",
-  isLoggedIn,
-  hasRole(["admin"]),
-  limiter,
-  fileController.updateFile
-);
-router.delete(
-  "/videos/:id",
-  isLoggedIn,
-  hasRole(["admin"]),
-  limiter,
-  fileController.deleteFile
-);
+router.post("/videos", mCreateVideo, createFile);
+router.get("/videos",mFindVideo,  getAllFile);
+router.put("/videos/:id", mUpdateVideo, updateFile);
+router.delete("/videos/:id", mDeleteVideo, deleteFile);
 
 module.exports = router;
