39cb275a244cb050d9aa6a2e9b8fd7b52c04950e
Javohir Muhammadiyev <138434351+JavohirDevloper@users.noreply.github.com>
2024-03-17 14:07:22 +0500

    user router

---
+++ backend/src/routers/user.router.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getUserById,
+  updateUser,
+  deleteUser,
+  register,
+  login,
+  getUserMe,
+  getAllUser,
+  createUser,
+} = require("../controllers/user.controller");
+const isLoggedIn = require("../shared/auth/isLoggedIn");
+const hasRole = require("../shared/auth/hasRole");
+const isMongoId = require("../shared/validator/isMongoId");
+
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 30,
+  message:
+    "Foydalanuvchi hajmi limitga yetdi. Iltimos, keyinroq harakat qiling.",
+});
+
+const mCreateUserAdmin = [isLoggedIn, limiter, hasRole(["admin"])];
+const mUserAdminFind = [isLoggedIn, limiter, hasRole(["admin"])];
+const mUserAdminFindById = [isLoggedIn, limiter, hasRole(["admin"])];
+const mUserGetMe = [isLoggedIn, limiter, hasRole(["student"])];
+const mDeleteMe = [isLoggedIn, limiter, hasRole(["student"])];
+const mUserAdminDelete = [isLoggedIn, isMongoId, limiter, hasRole(["admin"])];
+const mUserUpdateMe = [isLoggedIn, limiter, hasRole(["student"])];
+const mUserUpdateAdmin = [isLoggedIn, isMongoId, limiter, hasRole(["admin"])];
+
+router.post("/user/register", register);
+router.post("/user/login", login);
+router.post("/user", createUser );
+router.get("/user", mUserAdminFind, getAllUser);
+router.get("/user/:id", mUserAdminFindById, getUserById);
+router.get("/user/me",mUserGetMe, getUserMe);
+router.put("/user/:id", mUserUpdateAdmin, updateUser);
+router.put("/user/me", mUserUpdateMe);
+router.delete("/user/:id", mUserAdminDelete, deleteUser);
+router.delete("/user/me", mDeleteMe);
+module.exports = router;
