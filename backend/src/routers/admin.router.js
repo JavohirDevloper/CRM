b43578f9ac36728a11b1cb68a3aea5a9dcd86efb
Javohir Muhammadiyev <138434351+JavohirDevloper@users.noreply.github.com>
2024-03-17 22:32:13 +0500

    admin router

--- backend/src/routers/admin.router.js
+++ backend/src/routers/admin.router.js
@@ -7,17 +7,22 @@ const {
   deleteAdmin,
   loginAdmin,
   getFindById,
+  registerAdmin,
 } = require("../controllers/admin.controller");
 const isLoggedIn = require("../shared/auth/isLoggedIn");
 const hasRole = require("../shared/auth/hasRole");
 const isMongoId = require("../shared/validator/isMongoId");
 
-const mGetAllAdmins = [isLoggedIn, hasRole(["admin"])];
-const mGetByIdAdmin = [isLoggedIn, isMongoId, hasRole(["admin"])];
-const mUpdateAdmin = [isLoggedIn, isMongoId, hasRole(["admin"])];
-const mDeleteAdmin = [isLoggedIn, isMongoId, hasRole(["admin"])];
+const mRegisterAdmin = [isLoggedIn, hasRole(["super_admin"])];
+const mCreateAdmin = [isLoggedIn, hasRole(["super_admin"])];
+const mGetAllAdmins = [isLoggedIn, hasRole(["admin", "super_admin"])];
+const mGetByIdAdmin = [isLoggedIn, isMongoId, hasRole(["admin", "super_admin"])];
+const mUpdateAdmin = [isLoggedIn, isMongoId, hasRole(["super_admin"])];
+const mDeleteAdmin = [isLoggedIn, isMongoId, hasRole(["super_admin"])];
 
 router.post("/admin/login", loginAdmin);
+router.post("/admin/register", mRegisterAdmin, registerAdmin);
+router.post("/admin", mCreateAdmin, createAdmin);
 router.get("/admin", mGetAllAdmins, getAllAdmins);
 router.get("/admin/:id", mGetByIdAdmin, getFindById);
 router.put("/admin/:id", mUpdateAdmin, updateAdmin);
