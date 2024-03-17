d7418179aedbc8c68d67dfcd37fcf0ace4cf268d
Javohir Muhammadiyev <138434351+JavohirDevloper@users.noreply.github.com>
2024-03-17 14:05:06 +0500

    notification

---
+++ frontend/src/components/Notfication/Notfication.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import "./notfication.css"
+const Notfication = () => {
+  return (
+    <div>Notfication</div>
+  )
+}
+
+export default Notfication
