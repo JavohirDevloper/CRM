6125744e9808ddc2a933354f09cc1daef8c2afa5
Javohir Muhammadiyev <138434351+JavohirDevloper@users.noreply.github.com>
2024-03-20 01:12:21 +0500

    notification router

--- backend/src/routers/notification.router.js
+++ backend/src/routers/notification.router.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const rateLimit = require("express-rate-limit");
 const router = express.Router();
-const {
-
-} = require("../controllers/file.controller.js");
 const isLoggedIn = require("../shared/auth/isLoggedIn");
 const hasRole = require("../shared/auth/hasRole");
 const isMongoId = require("../shared/validator/isMongoId");
-const { createNotification, getAllNotifications, getNotificationById, updateNotification, deleteNotification } = require("../controllers/notification.controller.js");
+const {
+  createNotification,
+  getAllNotifications,
+  getNotificationById,
+  updateNotification,
+  deleteNotification,
+} = require("../controllers/notification.controller.js");
 
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000,
@@ -16,15 +19,38 @@ const limiter = rateLimit({
     "Foydalanuvchi hajmi limitga yetdi. Iltimos, keyinroq harakat qiling.",
 });
 
-const mCreateNotification = [isLoggedIn, limiter, hasRole(["admin", "super_admin"])];
-const mFindNotification = [isLoggedIn, limiter, hasRole(["student", "admin", "super_admin", "teacher"])];
-const mFindByIdNotification = [isLoggedIn, limiter,isMongoId, hasRole(["student", "admin", "super_admin", "teacher"])];
-const mUpdateNotification = [isLoggedIn, limiter, isMongoId, hasRole(["admin", "super_admin"])];
-const mDeleteNotification = [isLoggedIn, limiter, isMongoId, hasRole(["admin", "super_admin"])];
+const mCreateNotification = [
+  isLoggedIn,
+  limiter,
+  hasRole(["admin", "super_admin"]),
+];
+const mFindNotification = [
+  isLoggedIn,
+  limiter,
+  hasRole(["student", "admin", "super_admin", "teacher"]),
+];
+const mFindByIdNotification = [
+  isLoggedIn,
+  limiter,
+  isMongoId,
+  hasRole(["student", "admin", "super_admin", "teacher"]),
+];
+const mUpdateNotification = [
+  isLoggedIn,
+  limiter,
+  isMongoId,
+  hasRole(["admin", "super_admin"]),
+];
+const mDeleteNotification = [
+  isLoggedIn,
+  limiter,
+  isMongoId,
+  hasRole(["admin", "super_admin"]),
+];
 
 router.post("/notification", mCreateNotification, createNotification);
-router.get("/notification",mFindNotification,  getAllNotifications);
-router.get("/notification/:id",mFindByIdNotification,  getNotificationById);
+router.get("/notification", mFindNotification, getAllNotifications);
+router.get("/notification/:id", mFindByIdNotification, getNotificationById);
 router.put("/notification/:id", mUpdateNotification, updateNotification);
 router.delete("/notification/:id", mDeleteNotification, deleteNotification);
 
